5f531284b70ea245c79f076a14e7faf4cace8f66
HazelC <akinyiagnes.97@gmail.com>
2023-07-25 14:16:31 +0300

    Ft: Handle Deletion and Update functions

--- src/api/schemas/index.ts
+++ src/api/schemas/index.ts
@@ -1,38 +1,3 @@
-// import gql from "graphql-tag";
-
-// const typeDefs = gql`
-//   "Type for a task on the Column"
-//   type Task {
-//     id: ID!
-//     Description: String!
-//   }
-//   "Column that is displayed on the column"
-//   type Column {
-//     id: ID!
-//     Title: String
-//     TaskId: [ID]
-//   }
-
-//   type Query {
-//     "get all the Columns"
-//     Columns: [Column]
-//     "get a single Column"
-//     Column(id: ID!): Column!
-//     "get all tasks"
-//     Tasks: [Task!]
-//     "get single task"
-//     Task(id: ID!): Task!
-//   }
-
-//   type Mutation {
-//     "Mutation to add a column to Board"
-//     addColumn(Title: String): Column
-//     "Mutation to add a task to a column"
-//     addTask(Description: String): Task
-//   }
-// `;
-// export default typeDefs;
-
 import gql from "graphql-tag";
 
 const typeDefs = gql`
@@ -65,7 +30,15 @@ const typeDefs = gql`
     "Mutation to add a task to a column"
     addTask(Description: String, ColumnId: String!): Task
     "Update Column"
-    updateColumn(id: ID!, Title: String, TaskId: [ID]): Column
+    updateColumn(id: ID!, Title: String): Column
+    "Update Task"
+    updateTask(id: ID!, Description: String, ColumnId: String): Task
+    "Delete Column"
+    deleteColumn(id: ID!): Boolean
+    "Delete Task"
+    deleteTask(id: ID!): Boolean
+    "Clear column"
+    clearColumnTasks(ColumnId: String!): Boolean
   }
 `;
 export default typeDefs;
